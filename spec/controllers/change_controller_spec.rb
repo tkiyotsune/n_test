09db0e7bdf8cb97073daf52dd52878969631a42e
tkiyotsune <tkiyotsune@gmail.com>
2022-10-20 21:49:04 +0900

    rspecでのテスト用にrailsディレクトリ追加

--- spec/controllers/change_controller_spec.rb
+++ spec/controllers/change_controller_spec.rb
@@ -1,8 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe ChangeController  do
-    it "number_change" do
-      # ChangeController.new.n_change("三億五百万二千").to eq "305002000"
-      expect(ChangeController.n_change("三億五百万二千")).to eq "305002000"
+    # 漢数字->英数字
+    it "一" do
+      expect(ChangeController.n_change('["一"]')).to eq 1
     end
+
+    it "二十三" do
+      expect(ChangeController.n_change('["二十三"]')).to eq 23
+    end
+
+    it "三百五" do
+      expect(ChangeController.n_change('["三百五"]')).to eq 305
+    end
+
+    it "三億五百万二千" do
+      expect(ChangeController.n_change('["三億五百万二千"]')).to eq 305002000
+    end
+    
+    it "一兆三十億五千万" do
+      expect(ChangeController.n_change('["一兆三十億五千万"]')).to eq 1003050000000
+    end
+
+    # 英数字->漢数字
+    it "1" do
+      expect(ChangeController.n_change("1".to_s)).to eq "一"
+    end
+
+    it "10" do
+      expect(ChangeController.n_change("23".to_s)).to eq "二十三"
+    end
+
+    it "105" do
+      expect(ChangeController.n_change("305".to_s)).to eq "三百五"
+    end
+
+    it "305002000" do
+      expect(ChangeController.n_change("305002000".to_s)).to eq "三億五百万二千"
+    end
+    
+    it "1003050000000" do
+      expect(ChangeController.n_change("1003050000000".to_s)).to eq "一兆三十億五千万"
+    end
+
 end
